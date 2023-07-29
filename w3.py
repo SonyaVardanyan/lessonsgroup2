6f45901505161239688a0d3d2a7e703d5fda21d3
--replace-all <vsonya011@gmail.com>
2023-07-30 03:41:34 +0400

    xndir 30

--- w3.py
+++ w3.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # #19
-d1 = {'a': 100, 'b': 200, 'c':300}
-d2 = {'a': 300, 'b': 200, 'd':400, 'e':500} 
-for key in d2:
-    if key in d1:
-        d1[key]+=d2[key]
-    else:
-        d1[key]=d2[key]
-print(d1)
+# d1 = {'a': 100, 'b': 200, 'c':300}
+# d2 = {'a': 300, 'b': 200, 'd':400, 'e':500} 
+# for key in d2:
+#     if key in d1:
+#         d1[key]+=d2[key]
+#     else:
+#         d1[key]=d2[key]
+# print(d1)
 
 
-#23
+# # #23
 # data = [{'item': 'item1', 'amount': 400},
 #         {'item': 'item2', 'amount': 300},
 #         {'item': 'item1', 'amount': 750}]
@@ -25,3 +25,26 @@ print(d1)
 #         combined_data[item] = amount
 
 # print(combined_data)
+
+
+#30
+data = {'item1': 45.50, 'item2': 35, 'item3': 41.30, 'item4': 55, 'item5': 24}
+top1, top2, top3 = None, None, None
+for item, price in data.items():
+    if top1 is None or price > data[top1]:
+        top3 = top2
+        top2 = top1
+        top1 = item
+    elif top2 is None or price > data[top2]:
+        top3 = top2
+        top2 = item
+    elif top3 is None or price > data[top3]:
+        top3 = item
+print("Top three items in the shop:")
+print(f"{top1}: {data[top1]}")
+print(f"{top2}: {data[top2]}")
+print(f"{top3}: {data[top3]}")
+
+
+
+
