99578e799882f921c0bf49ea6d25c75c4ea48323
--replace-all <vsonya011@gmail.com>
2023-07-31 12:30:01 +0400

    ex 30 with sorted

--- w3.py
+++ w3.py
@@ -28,22 +28,28 @@
 
 
 #30
+# data = {'item1': 45.50, 'item2': 35, 'item3': 41.30, 'item4': 55, 'item5': 24}
+# top1, top2, top3 = None, None, None
+# for item, price in data.items():
+#     if top1 is None or price > data[top1]:
+#         top3 = top2
+#         top2 = top1
+#         top1 = item
+#     elif top2 is None or price > data[top2]:
+#         top3 = top2
+#         top2 = item
+#     elif top3 is None or price > data[top3]:
+#         top3 = item
+# print("Top three items in the shop:")
+# print(f"{top1}: {data[top1]}")
+# print(f"{top2}: {data[top2]}")
+# print(f"{top3}: {data[top3]}")
+
 data = {'item1': 45.50, 'item2': 35, 'item3': 41.30, 'item4': 55, 'item5': 24}
-top1, top2, top3 = None, None, None
-for item, price in data.items():
-    if top1 is None or price > data[top1]:
-        top3 = top2
-        top2 = top1
-        top1 = item
-    elif top2 is None or price > data[top2]:
-        top3 = top2
-        top2 = item
-    elif top3 is None or price > data[top3]:
-        top3 = item
-print("Top three items in the shop:")
-print(f"{top1}: {data[top1]}")
-print(f"{top2}: {data[top2]}")
-print(f"{top3}: {data[top3]}")
+sorted_data = sorted(data.items())
+top_three = sorted_data[:3]
+
+print("Top three items in the shop:" ,top_three)
 
 
 
