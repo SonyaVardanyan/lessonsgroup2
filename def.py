7baa03181c469bf0ff378c98c297413fe3fa9d27
--replace-all <vsonya011@gmail.com>
2023-07-31 12:10:57 +0400

    def function.

---
+++ def.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# #1
+# def my_upper(input_str):
+#     return input_str.upper()
+
+# a= input("Enter a string: ")
+# result = my_upper(a)
+# print(result)
+
+# #2
+# def my_lower(input_str):
+#     return input_str.lower()
+
+# a= input("Enter a string: ")
+# result = my_lower(a)
+# print(result)
+
+# #3
+# def mmin(a):
+#     min_num=a[0]
+#     for el in a:
+#         if el < min_num:
+#             min_num=el
+#     print (min_num)
+# nums=[2,5,6,7,8,1]
+# mmin(nums)
+
+# #4
+# def mmax(a):
+#     max_num=a[0]
+#     for el in a:
+#         if el > max_num:
+#             max_num=el
+#     print (max_num)
+# nums=[2,5,6,7,8,1]
+# mmax(nums)
